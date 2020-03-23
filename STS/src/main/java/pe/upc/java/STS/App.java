d105c5fc1458fc825961f88821c9d5a00d794ed1
javacf <flores.orihuela@gmail.com>
2020-03-22 23:15:53 -0500

    First

---
+++ STS/src/main/java/pe/upc/java/STS/App.java
@@ -0,0 +1,13 @@
+package pe.upc.java.STS;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
